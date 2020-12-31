7e93a1da121ff4a96e5a60450ecc97132220df61
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-12-31 23:53:40 +0530

    work: Created User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other')
+    )
+
+    LANGUAGE_ENGLISH = 'en'
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, 'English'),
+    )
+
+    AMERICAN_DOLLARS = 'usd'
+    INDIAN_RUPEES = 'inr'
+
+    CURRENCY_CHOICES = (
+        (AMERICAN_DOLLARS, 'USD'),
+        (INDIAN_RUPEES, 'INR')
+    )
+
+    bio = models.TextField(default="", blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(max_length=10, null=True, choices=GENDER_CHOICES, blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, default=LANGUAGE_ENGLISH)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=2, default=AMERICAN_DOLLARS)
+    superhost = models.BooleanField(default=False)
+    
+
+
