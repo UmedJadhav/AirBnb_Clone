799a446aebcce495dd68edf2e8b17422302120b9
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-01 00:31:52 +0530

    work: Finished up the users admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Profile", {
+            "fields": (
+                'avatar',
+                'gender',
+                'bio',
+                'language',
+                'currency',
+                'superhost'
+            ),
+        }),
+    )
+    
+
