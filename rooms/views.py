6270e16ad493e21b659c23aa42d7869fda112c7c
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-02 23:41:08 +0530

    created basic List view for room listings

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import ListView, DetailView, View, UpdateView, FormView
+from django.shortcuts import render, redirect, reverse
+from django.core.paginator import Paginator
+from . import models #, forms
 
-# Create your views here.
+
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 12
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
