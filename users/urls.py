b0a37be2fad18f2fa9581b987e18cc0c17f0514e
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-03 18:38:57 +0530

    new: Added Github OAuth Login

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('signup', views.SignUpView.as_view(), name='signup'),
     path("verify/<str:key>/", views.complete_verification, name="complete-verification"),
+    path("login/github/", views.github_login, name="github-login"),
+    path("login/github/callback/", views.github_callback, name="github-callback"),
 ]
