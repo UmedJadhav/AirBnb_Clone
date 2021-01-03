dde203f9479807b85d2453d81de95035aaf73f8a
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-03 13:37:26 +0530

    new: Created the Login form and view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import os
+import requests
+from django.utils import translation
+from django.http import HttpResponse
+from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import FormView, DetailView, UpdateView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.files.base import ContentFile
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from . import forms, models, mixins
 
-# Create your views here.
+
+class LoginView(mixins.LoggedOutOnlyView, FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password) # Here I use email as username which is why I didnt use Login View
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
+
+
+def log_out(request):
+    messages.info(request, f"See you later")
+    logout(request)
+    return redirect(reverse("core:home"))
