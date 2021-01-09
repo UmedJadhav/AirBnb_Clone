4758e2b362a399a937d28bd89eaa5ac8659a45aa
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-09 12:33:15 +0530

    work: Created BookedDay model

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import Reservation, BookedDay
 
 # Register your models here.
 @admin.register(Reservation)
@@ -17,3 +17,8 @@ class ReservationAdmin(admin.ModelAdmin):
     list_filter = (
         'status',
     )
+
+@admin.register(BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+
+    list_display = ("day", "reservation")
