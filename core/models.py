838a4c218dd3b98c973c657edd4976c13fbbb6c3
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-11 12:25:56 +0530

    work: Created common Manager

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from . import managers
 
 
 # Create your models here.
 class Time_stamped_Model(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
 
     class Meta:
         abstract = True  # So that this model is not in DB
