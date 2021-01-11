b37eaa57079464597c63f310ee30823461d3266a
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-11 12:28:11 +0530

    work: Created form to collect reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,18 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core.models import Time_stamped_Model
 
 
 # Create your models here.
 class Review(Time_stamped_Model):
     review = models.TextField()
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+    accuracy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    check_in = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     user = models.ForeignKey('users.User', related_name='reviews', on_delete=models.CASCADE)
     room = models.ForeignKey('rooms.Room', related_name='reviews', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created']
 
     def __str__(self): 
         return f'{self.review} - {self.room}'
