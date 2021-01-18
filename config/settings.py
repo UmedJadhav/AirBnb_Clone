1728dbc54ebbe241f279deb6c7c149d6c1642e92
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-18 09:40:59 +0530

    chore: minor changes for EB

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p2vvto_2=14w%mv)-wdjjz@c4b(+v9rm++hiqht3d!9)g-rv)@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.elasticbeanstalk.com']
 
 
 # Application definition
